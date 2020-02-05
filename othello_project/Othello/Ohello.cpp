7665b70a00f6d790700e271598d207e0d84d34e3
furukawa-jun <jun_furukawa@ewest.co.jp>
2020-02-05 09:25:18 +0900

    追加

--- othello_project/Othello/Ohello.cpp
+++ othello_project/Othello/Ohello.cpp
@@ -1,6 +1,36 @@
-﻿#include "OthelloLib.h"
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include "OthelloLib.h"
+#include "function.h"
 
 int main() {
+	
+	//オセロ盤面の石配置配列の定義
+	int board[8][8] = {};
 
+	/* 1.石の初期配置 */
+	//【提供関数】盤面の石の配置を初期化する(全て未配置ではなくゲーム開始時点の状態)
+	initializeBoard(*board, 64);
+
+	/* 2.盤面の表示 */
+	//【提供関数】オセロの盤面を描画する(レイアウトから全て描画される)
+	printBoard(*board, 64);
+
+	/* 3.ゲーム開始時の表示 */
+	game_start();
+
+	/* 4.現在のプレイヤーの表示 */
+	//game_progress(*board);
+
+	/*
+	for (int i = 0; i < 8; i++)
+	{
+		for (int v = 0; v < 8; v++)
+		{
+			printf("%d", board[i][v]);
+		}
+		printf("\n");
+	}
+	*/
 	return 0;
 }
